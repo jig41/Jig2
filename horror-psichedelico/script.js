b5c1b9f2187c4c23dab80a361bfb905b56a09b75
jig41 <giovaniburloni@gmail.com>
2025-09-03 14:00:34 +0200

    Update script.js

--- horror-psichedelico/script.js
+++ horror-psichedelico/script.js
@@ -12,7 +12,7 @@ const phrases = [
 
 let index = 0;
 
-document.body.addEventListener('touchstart', () => {
+document.body.addEventListener('onClick', () => {
   title.style.transform = `scale(${1 + Math.random() * 0.5}) rotate(${Math.random() * 20 - 10}deg)`;
   title.style.color = `hsl(${Math.random() * 360}, 100%, 50%)`;
   message.textContent = phrases[index % phrases.length];
